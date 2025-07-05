c5d5761a1d412ef9c5b61f304f708f37bdf752b4
wangzhen0518 <wangzhen0518@126.com>
2025-07-05 18:15:44 +0800

    deal PING command

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 #![allow(unused_imports)]
-use std::net::TcpListener;
+use std::{io::Write, net::TcpListener};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut _stream) => {
+                _stream.write_all(b"+PONG\r\n").unwrap();
                 println!("accepted new connection");
             }
             Err(e) => {
